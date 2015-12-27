46320431d806b77ca7f22e249450aec846874e3c
Kirk Roerig <mr.possoms@gmail.com>
2015-12-27 20:17:01 +0000

    Sensor test stuff

--- sensors/sensorTest.cxx
+++ sensors/sensorTest.cxx
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <assert.h>
+#include <indicurses.h>
 
 #include "system.h"
 #include "aggergate.h"
@@ -14,28 +15,38 @@ int main(int argc, char* argv[])
 
 	int res = senInit(argv[1], argv[2], "./../imu.cal");
 	printf("Res %d\n", res);
+	icInit();
+
 
 	assert(!res);
 
 	while(1){
+		char buf[32] = {};
 		printf("Begin\n");
 		senUpdate(&SYS.body);
 
 		vec3f_t *posMes = &SYS.body.measured.position, *posEst = &SYS.body.estimated.position;
 		vec3f_t *velMes = &SYS.body.measured.velocity, *velEst = &SYS.body.estimated.velocity;
-		printf(
+	
+		sprintf(
+			buf,
 			"Position mes:(%f, %f, %f) est:(%f, %f, %f)\n",
 			posMes->x, posMes->y, posMes->z,
 			posEst->x, posEst->y, posEst->z
 		);
-		printf(
+		icText(2, 2, buf);
+
+		sprintf(
+			buf,
 			"Velocity mes:(%f, %f, %f) est:(%f, %f, %f)\n",
 			velMes->x, velMes->y, velMes->z,
 			velEst->x, velEst->y, velEst->z
 		);
+		icText(2, 4, buf);
 
 		timerUpdate();
 		usleep(1000);
+		icPresent();
 	}
 
 	return 0;
