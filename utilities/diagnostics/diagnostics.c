8f605b43940560db4d88c20a8b4c61a67ffd144c
kirk <kirk@PossomServer.hsd1.mi.comcast.net>
2016-01-11 00:05:30 -0500

    :-/

--- utilities/diagnostics/diagnostics.c
+++ utilities/diagnostics/diagnostics.c
@@ -24,40 +24,11 @@
 static void* handler(void* params)
 {
 	int listenfd = *((int*)params); // listening fd
-	int ci = 0;                     // connection index
-	int connections[50] = {};
-	int maxFd = 0, usedConnections = 0;
-	struct timeval timeout = { 0, 10000 };
-
+	
 	while(1){
 		int newConnection = accept(listenfd, NULL, NULL);
-
-		if(newConnection > 0){
-			connections[ci++] = newConnection;
-			if(ci > usedConnections) ci = usedConnections;
-			ci %= (sizeof(connections) / sizeof(int));
-			if(newConnection > maxFd) maxFd = newConnection;
-		}
-
-		fd_set readFd;
-		FD_ZERO(&readFd);
-		for(int i = usedConnections; i--;){
-			FD_SET(connections[ci], &readFd);
-		}
-
-		if(select(maxFd + 1, &readFd, 0, 0, &timeout) < 0){
-			continue; // time-out occured
-		}
-
-		// for each connection that requested something
-		for(int i = usedConnections; i--;){
-			int fd = connections[i];
-			char byte;
-			while(read(fd, &byte, 1));
-			if(FD_ISSET(fd, &readFd)){
-				write(fd, &SYS.body, sizeof(fusedObjState_t));
-			}
-		}
+		write(newConnection, &SYS.body, sizeof(fusedObjState_t));
+		close(newConnection);
 	}
 
 	return NULL;
@@ -79,8 +50,8 @@ int diagHost(short port)
 	try(listen(listenfd, 10), "Could not begin listening");
 
 	// set the listen fd to non-blocking
-	int flags = fcntl(listenfd, F_GETFD);
-	fcntl(listenfd, F_SETFD, O_NONBLOCK | flags);
+	//int flags = fcntl(listenfd, F_GETFD);
+	//fcntl(listenfd, F_SETFD, O_NONBLOCK | flags);
 
 	try(pthread_create(&serverThread, NULL, handler, &listenfd), "Could not spawn diag server thread");
 
