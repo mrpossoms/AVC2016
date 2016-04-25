3311f4753f696cde754f0af142b4808bc49b9c59
Kirk Roerig <kirk.roerig@modustri.com>
2016-04-25 09:39:30 -0400

    Added kill button to AVC app

--- utilities/AVC Controller/AVC Controller/ViewController.m
+++ utilities/AVC Controller/AVC Controller/ViewController.m
@@ -13,6 +13,7 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+
 #define STORED_IP_ADDR @"LAST-IP-ADDRESS"
 
 @interface ViewController ()
@@ -113,6 +114,39 @@ void transmit(){
     [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc]
                                      initWithTarget:self
                                      action:@selector(dismiss)]];
+}
+
+- (IBAction)didTapKill:(id)sender {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        int sockfd;
+        if((sockfd = socket(AF_INET, SOCK_STREAM, 0)))
+        {
+            struct sockaddr_in addr = *HOST_ADDRESS;
+            addr.sin_port = htons(1339);
+
+            if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+            {
+                UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error"
+                                                                               message:@"Failed to connect to host. Service or device might be down."
+                                                                        preferredStyle:UIAlertControllerStyleAlert];
+                [alert addAction:[UIAlertAction actionWithTitle:@"OK"
+                                                          style:UIAlertActionStyleCancel
+                                                        handler:^(UIAlertAction * _Nonnull action) { }]];
+                [self presentViewController:alert animated:YES completion:^{ }];
+                close(sockfd);
+
+                dispatch_async(dispatch_get_main_queue(), ^{ [self.resolvingIndicator stopAnimating]; });
+                return;
+            }
+
+            uint32_t action = 4; // tell the daemon to start up the main AVC program
+
+            write(sockfd, &action, sizeof(action));
+            close(sockfd);
+
+            dispatch_async(dispatch_get_main_queue(), ^{ [self.resolvingIndicator stopAnimating]; });
+        }
+    });
 }
 
 - (IBAction)didTapRun:(id)sender {
