dd4a718d1b7accc62c9d3011197fe0a8035ebf02
kirk <kirk@PossomServer.hsd1.mi.comcast.net>
2016-04-30 11:24:43 -0400

    Fixed compile errors

--- decision/agents/agentThrottle.c
+++ decision/agents/agentThrottle.c
@@ -16,7 +16,7 @@ static float utility(agent_t* current, void* args)
 static void* action(agent_t* lastState, void* args)
 {
 	gpsWaypointCont_t* waypoint = SYS.route.currentWaypoint;
-	vec3f_t delta = vec3fSub(&SYS.body.measured.position, &waypoint->self.location);
+	vec3f_t delta = vec3fSub(&SYS.body.measured.sensors.gps, &waypoint->self.location);
 
 	// do stuff here, choose a successor state if appropriate
 	if(SYS.route.currentWaypoint && SYS.body.hasGpsFix && vec3fMag(&delta) > 8){
