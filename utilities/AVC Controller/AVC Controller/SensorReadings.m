6f0da2de1f060a1e0b393047526148669efc9255
Kirk Roerig <kirk.roerig@modustri.com>
2016-06-06 00:42:31 -0400

    Updated app

--- utilities/AVC Controller/AVC Controller/SensorReadings.m
+++ utilities/AVC Controller/AVC Controller/SensorReadings.m
@@ -10,31 +10,6 @@
 
 @implementation SensorReadings
 
-- (NSString*)stringForVecF:(vec3f_t)v
-{
-    return [NSString stringWithFormat:@"{ %0.3f, %0.3f, %0.3f }", v.x, v.y, v.z];
-}
-
-- (NSString*)stringForVecI:(vec3i16_t)v
-{
-    return [NSString stringWithFormat:@"{ %d, %d, %d }", v.x, v.y, v.z];
-}
-
-- (NSString*)mesPosition
-{
-    return [self stringForVecF:self.data.pose.pos];
-}
-
-
-- (NSString*)estPosition
-{
-    return [self stringForVecF:self.data.pose.pos];
-}
-
-- (NSString*)estHeading
-{
-    return [self stringForVecF:self.data.pose.heading];
-}
 
 - (NSString*)hasGpsFix
 {
